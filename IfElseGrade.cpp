66bb3d2be960243a44546c06ef4bd53d98f98492
jessacamenade <camenadejessa_bsit@plmun.edu.ph>
2025-11-03 15:09:29 +0800

    Add grade evaluation program using if...else

--- IfElseGrade.cpp
+++ IfElseGrade.cpp
@@ -1,3 +1,4 @@
+//Lab 3 – Two-Way Selection (if...else Statement)
 #include <iostream>
 using namespace std;
 int main()
@@ -16,3 +17,27 @@ int main()
         
     return 0;
 }
+
+//Lab 4 – Compound Statements
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int grade;
+    
+    cout << "Enter Grade(0-100): ";
+    cin >> grade;
+    
+    if (grade>=75){
+        cout << "Result: Pass\n";
+        cout << "Congratulations!\n";
+        cout << "Keep up the work.";
+    } else {
+        cout << "Result: Fail";
+    }
+   
+    return 0;
+}
+
